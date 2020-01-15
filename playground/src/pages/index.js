789d90bae4726bb19898d81a3e2fc7b0a7cbcb4e
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-15 11:10:15 -0800

    Playing around with adding text

--- playground/src/pages/index.js
+++ playground/src/pages/index.js
@@ -81,16 +81,19 @@ const Box = () => {
 }
 
 export default () => (
-  <Canvas camera={{position: [0, 0, 5]}} onCreated={({ gl }) => {
-    gl.shadowMap.enabled = true;
-    gl.shadowMap.type = THREE.PCFSoftShadowMap;
-  }}>
-    <ambientLight intensity={0.5} />
-    <spotLight position={[15, 20, 5]} penumbra={1} castShadow /> 
-    <fog attach="fog" args={["black", 10, 25]} />
-    <Controls />
-    {/* <Box /> */}
-    {/* <Plane /> */}
-    <SpaceShip />
-  </Canvas>
+  <>
+    <Canvas camera={{position: [0, 0, 5]}} onCreated={({ gl }) => {
+      gl.shadowMap.enabled = true;
+      gl.shadowMap.type = THREE.PCFSoftShadowMap;
+    }}>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[15, 20, 5]} penumbra={1} castShadow /> 
+      <fog attach="fog" args={["black", 10, 25]} />
+      <Controls />
+      {/* <Box /> */}
+      {/* <Plane /> */}
+      <SpaceShip />
+    </Canvas>
+    <h1>Andrew Dinh</h1>
+  </>
 )
