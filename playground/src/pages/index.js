8a599d0e92772dc4edeef2871a31b1f3b89eab56
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-14 17:03:53 -0800

    Soft reset to see if files are removed

--- playground/src/pages/index.js
+++ playground/src/pages/index.js
@@ -1,12 +1,22 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Canvas, extend, useThree, useRender } from 'react-three-fiber'
 import { useSpring, a } from 'react-spring/three'
 
 import './style.css'
 
 extend({ OrbitControls });
+
+const SpaceShip = () => {
+  const [model, setModel] = useState();
+  useEffect(() => {
+    new GLTFLoader().load('/scene.gltf', setModel);
+  })
+
+  return model ? <primitive object={model.scene} /> : null;
+}
 
 const Controls = () => {
   const orbitRef = useRef();
@@ -79,5 +89,6 @@ export default () => (
     <Controls />
     <Box />
     <Plane />
+    <SpaceShip />
   </Canvas>
 )
